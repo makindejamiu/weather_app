694fac7dc538cb6a8faf11b364489a3276065cc9
Jamiu Makinde <successmakinde@gmail.com>
2021-03-01 19:10:58 +0100

    the weather app ui

---
+++ lib/main.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Weather App',
+      home: Home(),
+    ),
+  );
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var temp;
+  var description;
+  var currently;
+  var humidity;
+  var windSpeed;
+
+  Future getWeather() async {
+    http.Response responce = await http.get(
+        'http://api.openweathermap.org/data/2.5/weather?q=lagos&appid=db5b5d60680134a2b79294497bbe4b5e');
+    var result = jsonDecode(responce.body);
+
+    setState(() {
+      this.temp = result['main']['temp'];
+      this.description = result['weather'][0]['description'];
+      this.currently = result['weather'][0]['main'];
+      this.humidity = result['main']['humidity'];
+      this.windSpeed = result['wind']['speed'];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.getWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height / 3,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.red[900],
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10.0),
+                  child: Text(
+                    'Currently In Lagos',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ),
+                Text(
+                  temp != null ? temp.toString() + '\u00B0' : 'loading',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.w600),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Text(
+                    currently != null ? currently.toString() : 'loading',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+              child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: ListView(
+              children: [
+                ListTile(
+                  leading: FaIcon(FontAwesomeIcons.thermometerHalf),
+                  title: Text('Temperature'),
+                  trailing: Text(temp != null ? temp.toString() + '\u00B0' : 'loading'),
+                ),
+                ListTile(
+                  leading: FaIcon(FontAwesomeIcons.cloud),
+                  title: Text('Weather'),
+                  trailing: Text(temp != null ? temp.toString() + '\u00B0' : 'loading'),
+                ),
+                ListTile(
+                  leading: FaIcon(FontAwesomeIcons.sun),
+                  title: Text('Humidity'),
+                  trailing: Text('\u00B0'),
+                ),
+                ListTile(
+                  leading: FaIcon(FontAwesomeIcons.wind),
+                  title: Text('Wind Speed'),
+                  trailing: Text('\u00B0'),
+                )
+              ],
+            ),
+          ))
+        ],
+      ),
+    );
+  }
+}
